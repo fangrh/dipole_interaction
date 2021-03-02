e09c085741eeada696c52bdbadd6c8d6776d2485
fangruihuan <fangrh5@mail2.sysu.edu.cn>
2021-03-03 01:27:30 +0800

    Edit with typora

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-pandoc -s Lindblad.md -t html -H night.css --katex -o Lindblad.md.html --filter pandoc-crossref
+pandoc -s Lindblad.md -t html -C night.css --katex -o Lindblad.md.html --filter pandoc-crossref
 git status
 git add --all
 git commit -m"Edit with typora"
