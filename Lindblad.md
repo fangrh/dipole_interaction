54855e48db251f596a477d774449e248a330a662
fangruihuan <fangrh5@mail2.sysu.edu.cn>
2021-03-03 22:59:30 +0800

    Edit with typora

--- Lindblad.md
+++ Lindblad.md
@@ -34,3 +34,5 @@ Here the $\alpha$ is the polarization mode.
 
 
 
+
+
--- push.sh
+++ push.sh
@@ -1,4 +1,3 @@
-pandoc Lindblad.md -c markdown1.css -t html  --katex -o Lindblad.md.html --filter pandoc-crossref -s
 git status
 git add --all
 git commit -m"Edit with typora"
